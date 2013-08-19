165c6e3166b8b41d3c63b70c67ae42812faa160b
Youngsam Park <yspark@gmail.com>
2013-08-19 17:46:58 -0400

    directory cleanup

---
+++ simul/one_anonymousdtn/routing/AnonymousEpidemicRouter.java
@@ -0,0 +1,58 @@
+/* 
+ * Copyright 2010 Aalto University, ComNet
+ * Released under GPLv3. See LICENSE.txt for details. 
+ */
+package routing;
+
+import core.Settings;
+
+/**
+ * Epidemic message router with drop-oldest buffer and only single transferring
+ * connections at a time.
+ */
+public class AnonymousEpidemicRouter extends ActiveRouter {
+	
+	/**
+	 * Constructor. Creates a new message router based on the settings in
+	 * the given Settings object.
+	 * @param s The settings object
+	 */
+	public AnonymousEpidemicRouter(Settings s) {
+		super(s);
+		//TODO: read&use epidemic router specific settings (if any)
+	}
+	
+	/**
+	 * Copy constructor.
+	 * @param r The router prototype where setting values are copied from
+	 */
+	protected AnonymousEpidemicRouter(AnonymousEpidemicRouter r) {
+		super(r);
+		//TODO: copy epidemic settings here (if any)
+	}
+			
+	@Override
+	public void update() {
+
+		
+		super.update();
+		if (isTransferring() || !canStartTransfer()) {
+			return; // transferring, don't try other connections yet
+		}
+		
+		// Try first the messages that can be delivered to final recipient
+		if (exchangeDeliverableMessages() != null) {
+			return; // started a transfer, don't try others (yet)
+		}
+		
+		// then try any/all message to any/all connection
+		this.tryAllMessagesToAllConnections();
+	}
+	
+	
+	@Override
+	public AnonymousEpidemicRouter replicate() {
+		return new AnonymousEpidemicRouter(this);
+	}
+
+}
